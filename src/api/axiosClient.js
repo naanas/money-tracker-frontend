e248a4b3407b615644c6e304db2d93b3984a9834
naanas <annasputra52@gmail.com>
2025-10-31 16:57:56 +0700

    xx

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,24 +1,19 @@
 import axios from 'axios';
-// Impor supabase client langsung
 import { supabase } from '../supabaseClient';
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// Interceptor untuk menambahkan token ke setiap request
-// Ubah menjadi fungsi 'async'
+
 axiosClient.interceptors.request.use(
   async (config) => {
-    // Jangan baca dari localStorage
-    // const sessionData = localStorage.getItem('sb-bqjuxfaitxjbsqlkypkf-auth-token');
     
-    // Minta sesi terbaru langsung dari Supabase
     const { data: { session }, error } = await supabase.auth.getSession();
 
     if (error) {
       console.error('Error getting session in axios interceptor:', error);
-      return config; // Lanjutkan tanpa token jika ada error
+      return config; 
     }
     
     if (session && session.access_token) {
